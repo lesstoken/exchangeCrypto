c5850cf17c7148b4017d344ebe5c797d6a7e2ab0
Pedro Henrique Sant'Anna <pedrohesm@gmail.com>
2022-01-27 18:42:45 -0300

    create token and settings test with chaijs

---
+++ truffle-config.js
@@ -0,0 +1,35 @@
+require("babel-register");
+require("babel-polyfill");
+require("dotenv").config();
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+  },
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: false,
+        runs: 200,
+      },
+    },
+  },
+};
